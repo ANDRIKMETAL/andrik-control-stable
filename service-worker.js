ce5dfa63c72b7557371916b74c7e5b2008e43dd7
ANDRIK Control <andrik-control@users.noreply.github.com>
2026-08-22 23:22:24 +0200

    ANDRIK Control — update website R560
    Release: R560

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const VERSION = '55.00-r559';
+const VERSION = '55.00-r560';
 const IS_CONTROL_HOST = self.location.hostname.toLowerCase() === 'control.andrikmetal.com';
 
 async function deleteControlCaches(){
@@ -42,7 +42,7 @@ if(IS_CONTROL_HOST){
     '/assets/app-links-v50.js?v=55.00-r213',
     '/assets/home-fast-r213.css?v=55.00-r213',
     '/assets/home-fast-r213.js?v=55.00-r213',
-    '/singles.html?v=55.00-r330', '/albums.html?v=55.00-r519', '/uk/albums.html?v=55.00-r519', '/sk/albums.html?v=55.00-r519', '/en/albums.html?v=55.00-r519', '/assets/albums-r446.css?v=55.00-r446', '/assets/albums-r446.js?v=55.00-r446', '/assets/albums-fast-library-r477.js?v=55.00-r519', '/assets/albums-trika-release-r519.css?v=55.00-r519', '/assets/singles-r316.css?v=330', '/assets/singles-r316.js?v=330',
+    '/singles.html?v=55.00-r560', '/albums.html?v=55.00-r519', '/uk/albums.html?v=55.00-r519', '/sk/albums.html?v=55.00-r519', '/en/albums.html?v=55.00-r519', '/assets/albums-r446.css?v=55.00-r446', '/assets/albums-r446.js?v=55.00-r446', '/assets/albums-fast-library-r477.js?v=55.00-r519', '/assets/albums-trika-release-r519.css?v=55.00-r519', '/assets/singles-r560.css?v=55.00-r560', '/assets/singles-r349.js?v=55.00-r349',
     '/assets/home-singles-nav-r330.css?v=55.00-r330', '/assets/home-singles-nav-r330.js?v=55.00-r330',
     '/assets/home-trika-live-r520.css?v=55.00-r520',
     '/assets/albums-fast-library-r477.css?v=55.00-r477', '/assets/albums-fast-library-r477.js?v=55.00-r477',
